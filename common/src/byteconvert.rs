d816de6883c39234f7071ba1327a988b52f10cdf
cook1e <vincent.schweiger@xolley.de>
2021-08-17 20:39:37 +0200

    tests

--- common/src/byteconvert.rs
+++ common/src/byteconvert.rs
@@ -7,7 +7,7 @@ pub fn convert(num: f64) -> String {
     if num < 1_f64 {
         return format!("{}{} {}", negative, num, "B");
     }
-    let delimiter = 1000_f64;
+    let delimiter = 1024_f64;
     let exponent = cmp::min(
         (num.ln() / delimiter.ln()).floor() as i32,
         (units.len() - 1) as i32,
@@ -18,4 +18,21 @@ pub fn convert(num: f64) -> String {
         * 1_f64;
     let unit = units[exponent as usize];
     format!("{}{} {}", negative, pretty_bytes, unit)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn kilobyte() {
+        assert_eq!(convert(1024 as f64), "1 kB")
+    }
+    #[test]
+    fn megabyte() {
+        assert_eq!(convert((1024*1024) as f64), "1 MB")
+    }
+    #[test]
+    fn gigabyte() {
+        assert_eq!(convert((1024*1024*1024) as f64), "1 GB")
+    }
 }
