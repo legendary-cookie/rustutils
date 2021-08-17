5fb79844af44e137b84ce26339fc7f12a045f9f0
cook1e <vincent.schweiger@xolley.de>
2021-08-17 17:59:50 +0200

    stuff

--- rget/src/main.rs
+++ rget/src/main.rs
@@ -29,35 +29,26 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
     /* REST OF THE STUFF */
     let client = reqwest::Client::new();
-
     println!("Will download {} to {}", url, path);
-
     // Reqwest setup
     let res = client
         .get(url)
         .send()
         .await
         .or(Err(format!("Failed to GET from '{}'", &url)))?;
-
     let total = res
         .content_length()
         .ok_or(format!("Failed to get content length from {}", &url))?;
-
     println!("Total size: {:?} MB", total / 1024 / 1024);
-
     let pb = ProgressBar::new(total);
     pb.set_style(ProgressStyle::default_bar()
         .template("{msg}\n{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({bytes_per_sec}, {eta})")
         .progress_chars("#>-"));
-
     pb.set_message(format!("Downloading {}", url));
-
     // Download chunks
     let mut file = File::create(path).or(Err(format!("Failed to create file '{}'", path)))?;
-
     let mut downloaded: u64 = 0;
     let mut stream = res.bytes_stream();
-
     while let Some(item) = stream.next().await {
         let chunk = item.or(Err(format!("Error while downloading file")))?;
         file.write(&chunk)
@@ -66,7 +57,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         downloaded = new;
         pb.set_position(new);
     }
-
     pb.finish_with_message(format!("Downloaded {} to {}", url, path));
 
     Ok(())
